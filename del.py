8192cf4641c4db1e825beb0c0312a54bf8d09006
Jianjie Luo <99809581+Jie-Shapingba@users.noreply.github.com>
2022-09-15 12:11:24 +1000

    Delete del.py

--- del.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-import os
-
-#os.chmod('/usr/local/adacnew/PinnacleSiteData/Scripts/HotScripts/Physics/LARK_Dose_Recon_v1.4/create_2dgr.Script',777)
-os.remove('/usr/local/adacnew/PinnacleSiteData/Scripts/HotScripts/Physics/LARK_Dose_Recon_v1.4/create_2dgr.Script')
-#os.remove('/usr/local/adacnew/PinnacleSiteData/Scripts/HotScripts/Physics/LARK_Dose_Recon_v1.4/create_poi.Script')
-os.remove('/usr/local/adacnew/PinnacleSiteData/Scripts/HotScripts/Physics/LARK_Dose_Recon_v1.4/temp/*.*')
